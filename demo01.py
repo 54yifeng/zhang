6bffa9f5fce5d855ff8e0b1a7de9fc66629547be
54yifeng <849457013@qq.com>
2020-04-10 18:52:02 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,44 @@
+"""
+print("你好") #字符串
+print(2333) #整数
+print(2.333) #小数
+print(True) #布尔值 True False
+print(()) #元组
+print([]) #数组
+print({}) #字典
+
+锄禾日当午
+汗滴禾下土
+
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻") #字符串的拼接
+print("哈哈"*100)
+print(((1+2)*100-99)/2)
+print(2<3)
+"""
+#变量
+#赋值
+# a = float(input("请输入: "))
+# b = float(input("请输入: "))
+# print("input获取的内容: ",a+b)
+
+#len()命令只支持：字符串、元组、数组、字典
+a = input("请输入: ")
+b = input("请输入: ")
+print("input获取的内容: ",len(a+b))
+print(a+b)
+
+# 数据格式的转换
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a = int(2333)
+# print(type(a))
+
+# b = str("哈哈哈")
+# print(type(b))
