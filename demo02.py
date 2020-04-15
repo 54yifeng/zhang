e80ef09a85f4bb7a26dad4df20c90999bf70573e
54yifeng <849457013@qq.com>
2020-04-15 20:58:14 +0800

    two

---
+++ demo02.py
@@ -0,0 +1,30 @@
+# 判断
+# a = 3
+# b = 4 
+# if a > b:    #必须有冒号
+#     print("a比b大")   #前面有四个空格
+# else:
+#     print("b更大")
+
+
+age = int(input("请输入年龄："))
+
+# if age > 60:
+#     print("退休")
+# elif age > 30:
+#     print("赚钱养家")
+# elif age >20:
+#     print("规划好未来")
+# else:
+#     print("好好学习")
+
+# if age >20 and age <= 30:
+#     print("22222222")
+# elif age > 30 and age <= 40:
+#     print("33333333")
+# elif age > 40 and age <= 50:
+#     print("44444444")
+# else:
+#     print("55555555")
+ 
+
